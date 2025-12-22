08f976817d42de7b90b0b822b366bfea3f3de1ea
Mahfujmazid47 <mahfujmazid47@gmail.com>
2025-12-22 11:37:03 +0600

    added post methode for todos table

--- src/server.ts
+++ src/server.ts
@@ -220,6 +220,30 @@ app.delete('/users/:id', async (req: Request, res: Response) => {
 
 
 
+
+// TODOs CRUD OPERATIONS
+// create a todos
+app.post("/todos", async (req: Request, res: Response) => {
+    const { user_id, title } = req.body;
+    try {
+        const result = await pool.query(`INSERT INTO todos(user_id, title) VALUES($1, $2) RETURNING *`, [user_id, title]);
+
+        res.status(201).json({
+            success: true,
+            message: "TODO created",
+            data: result.rows[0]
+        })
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+})
+
+
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
